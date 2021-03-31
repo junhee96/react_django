2cf17db62e0abaed03f84cc857ea445ba90a7601
junhee96 <tyyt0528@nate.com>
2021-04-01 07:27:30 +0900

    commit

---
+++ front/src/api.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+
+axios.defaults.baseURL = "http://127.0.0.1:8000/api"
+
+export default {
+    // 모든 글 불러오기
+    getAllPosts(){
+        return axios.get('/posts/')
+    },
+    // 글작성하기
+    createPost(data){
+        return axios.post('/posts/',data)
+    },
+    deletePost(id){
+        return axios.delete('/posts/'+String(id))
+    },
+    updatePost(data, id) {
+        console.log('업뎃해따')
+        return axios.put('/posts/'+String(id), data)
+    },
+}
